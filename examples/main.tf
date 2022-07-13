61163b41bbbeadcdab972dfc4daf14b28f6f7746
r.vasu <r.vasu@global.leaseweb.com>
2022-07-13 15:11:35 +0530

    BMA-5973 null and unnull ip

--- examples/main.tf
+++ examples/main.tf
@@ -12,15 +12,3 @@ provider "leaseweb" {}
 resource "leaseweb_dedicatedserver" "web01" {
   # reference = "web01"
 }
-
-resource "leaseweb_dedicatedserver" "web02" {
-  # reverse_lookup = "web02.example.com"
-}
-
-resource "leaseweb_dedicatedserver" "db01" {
-  # powered_on = true
-}
-
-resource "leaseweb_dedicatedserver" "db02" {
-  # dhcp_lease = "https://boot.netboot.xyz"
-}
--- leaseweb/api_client.go
+++ leaseweb/api_client.go
@@ -326,3 +326,41 @@ func removeDHCPLease(serverID string) error {
 
 	return nil
 }
+
+func nullIp(serverID string, IP string) error {
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/bareMetals/v2/servers/%s/ips/%s/null", leasewebAPIURL, serverID, IP), nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("X-Lsw-Auth", leasewebAPIToken)
+
+	response, err := leasewebClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		return fmt.Errorf("error nulling ip of the server, api response %v", response.StatusCode)
+	}
+
+	return nil
+}
+
+func unnullIp(serverID string, IP string) error {
+	request, err := http.NewRequest("POST", fmt.Sprintf("%s/bareMetals/v2/servers/%s/ips/%s/unnull", leasewebAPIURL, serverID, IP), nil)
+	if err != nil {
+		return err
+	}
+	request.Header.Set("X-Lsw-Auth", leasewebAPIToken)
+
+	response, err := leasewebClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusAccepted {
+		return fmt.Errorf("error unnulling server ip of the server, api response %v", response.StatusCode)
+	}
+
+	return nil
+}
