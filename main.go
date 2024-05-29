1c8e892df28ed8595a94fb9a45b52c15defabf63
Ryan Kes <ryan@ryankes.eu>
2024-05-29 19:03:32 +0200

    chore: remove code comments

--- main.go
+++ main.go
@@ -8,23 +8,11 @@ import (
 	"terraform-provider-leaseweb/internal/provider"
 )
 
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
 //go:generate terraform fmt -recursive ./examples/
-
-// Run the docs generation tool, check its repository for more information on how it works and how docs
-// can be customized.
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name leaseweb
 
 var (
-	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
 	version = "dev"
-
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
 )
 
 func main() {
@@ -34,11 +22,6 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// NOTE: This is not a typical Terraform Registry provider address,
-		// such as registry.terraform.io/hashicorp/hashicups. This specific
-		// provider address is used in these tutorials in conjunction with a
-		// specific Terraform CLI configuration for manual development testing
-		// of this provider.
 		Address: "registry.terraform.io/LeaseWeb/leaseweb",
 		Debug:   debug,
 	}
