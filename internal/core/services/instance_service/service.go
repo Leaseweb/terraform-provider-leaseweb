3686fb57eb2cb4297b5ad04335e9204a9a03a2f2
Ryan Kes <ryan@ryankes.eu>
2024-07-05 17:20:41 +0200

    feat: implement instanceRepository in instanceService

--- internal/core/services/instance_service/service.go
+++ internal/core/services/instance_service/service.go
@@ -1,38 +1,74 @@
 package instance_service
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
 	"terraform-provider-leaseweb/internal/core/domain/entity"
+	"terraform-provider-leaseweb/internal/core/ports"
 )
 
-type Service struct{}
+var ErrFailedToGetInstancesFromRepository = errors.New("failed to retrieve instances from repository")
+var ErrFailedToGetInstanceFromRepository = errors.New("failed to retrieve instance from repository")
+var ErrFailedToCreateInstance = errors.New("failed to create instance")
+var ErrFailedToUpdateInstance = errors.New("failed to update instance")
+var ErrFailedToDeleteInstance = errors.New("failed to delete instance")
+
+type Service struct {
+	instanceRepository ports.InstanceRepository
+}
 
 func (srv Service) GetAllInstances() (entity.Instances, error) {
-	return entity.Instances{}, nil
+	instances, err := srv.instanceRepository.GetAllInstances()
+	if err != nil {
+		return entity.Instances{}, ErrFailedToGetInstancesFromRepository
+	}
+
+	return instances, nil
 }
 
 func (srv Service) GetInstance(id uuid.UUID) (entity.Instance, error) {
-	return entity.Instance{}, nil
+	instance, err := srv.instanceRepository.GetInstance(id)
+	if err != nil {
+		return entity.Instance{}, ErrFailedToGetInstanceFromRepository
+	}
+
+	return instance, nil
 }
 
 func (srv Service) CreateInstance(instance entity.Instance) (
 	entity.Instance,
 	error,
 ) {
-	return entity.Instance{}, nil
+	instance, err := srv.instanceRepository.CreateInstance(instance)
+	if err != nil {
+		return entity.Instance{}, ErrFailedToCreateInstance
+	}
+
+	return instance, nil
 }
 
 func (srv Service) UpdateInstance(instance entity.Instance) (
 	entity.Instance,
 	error,
 ) {
-	return entity.Instance{}, nil
+	instance, err := srv.instanceRepository.UpdateInstance(instance)
+	if err != nil {
+		return entity.Instance{}, ErrFailedToUpdateInstance
+	}
+
+	return instance, nil
 }
 
 func (srv Service) DeleteInstance(id uuid.UUID) error {
+	err := srv.instanceRepository.DeleteInstance(id)
+	if err != nil {
+		return ErrFailedToDeleteInstance
+	}
+
 	return nil
 }
 
-func New() Service {
-	return Service{}
+func New(instanceRepository ports.InstanceRepository) Service {
+	return Service{instanceRepository: instanceRepository}
 }
--- internal/core/services/instance_service/service_test.go
+++ internal/core/services/instance_service/service_test.go
@@ -8,48 +8,164 @@ import (
 	"terraform-provider-leaseweb/internal/core/domain/entity"
 )
 
+type repositorySpy struct {
+	Error     error
+	Instances entity.Instances
+	Instance  entity.Instance
+}
+
+func (r repositorySpy) GetAllInstances() (entity.Instances, error) {
+	if r.Error != nil {
+		return entity.Instances{}, r.Error
+	}
+
+	return r.Instances, nil
+}
+
+func (r repositorySpy) GetInstance(id uuid.UUID) (entity.Instance, error) {
+	if r.Error != nil {
+		return entity.Instance{}, r.Error
+	}
+
+	r.Instance.Id = id
+	return r.Instance, nil
+}
+
+func (r repositorySpy) CreateInstance(instance entity.Instance) (
+	entity.Instance,
+	error,
+) {
+	if r.Error != nil {
+		return entity.Instance{}, r.Error
+	}
+
+	return r.Instance, nil
+}
+
+func (r repositorySpy) UpdateInstance(instance entity.Instance) (
+	entity.Instance,
+	error,
+) {
+	if r.Error != nil {
+		return entity.Instance{}, r.Error
+	}
+
+	return r.Instance, nil
+}
+
+func (r repositorySpy) DeleteInstance(id uuid.UUID) error {
+	if r.Error != nil {
+		return r.Error
+	}
+
+	return nil
+}
+
 func TestService_GetAllInstances(t *testing.T) {
-	instanceService := New()
+	t.Run(
+		"service passes back instances from repository",
+		func(t *testing.T) {
+			id := uuid.New()
+			instanceService := New(repositorySpy{Instances: entity.Instances{{Id: id}}})
 
-	got, err := instanceService.GetAllInstances()
+			got, err := instanceService.GetAllInstances()
 
-	assert.Nil(t, err)
-	assert.Equal(t, entity.Instances{}, got)
+			assert.Nil(t, err)
+			assert.Equal(t, id, got[0].Id)
+		},
+	)
+
+	t.Run("service passes back error from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{Error: ErrFailedToUpdateInstance})
+
+		_, err := instanceService.GetAllInstances()
+
+		assert.NotNil(t, err)
+	})
 }
 
 func TestService_GetInstance(t *testing.T) {
-	instanceService := New()
-	id, _ := uuid.NewUUID()
+	t.Run(
+		"service passes back instance from repository",
+		func(t *testing.T) {
+			id := uuid.New()
+			instanceService := New(repositorySpy{})
 
-	got, err := instanceService.GetInstance(id)
+			got, err := instanceService.GetInstance(id)
 
-	assert.Nil(t, err)
-	assert.Equal(t, entity.Instance{}, got)
+			assert.Nil(t, err)
+			assert.Equal(t, id, got.Id)
+		},
+	)
+
+	t.Run("service passes back error from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{Error: ErrFailedToUpdateInstance})
+
+		_, err := instanceService.GetInstance(uuid.New())
+
+		assert.NotNil(t, err)
+	})
 }
 
 func TestService_CreateInstance(t *testing.T) {
-	instanceService := New()
+	t.Run(
+		"service passes back instance from repository",
+		func(t *testing.T) {
+			id := uuid.New()
+			instanceService := New(repositorySpy{Instance: entity.Instance{Id: id}})
 
-	got, err := instanceService.CreateInstance(entity.Instance{})
+			got, err := instanceService.CreateInstance(entity.Instance{})
 
-	assert.Nil(t, err)
-	assert.Equal(t, entity.Instance{}, got)
+			assert.Nil(t, err)
+			assert.Equal(t, id, got.Id)
+		},
+	)
+
+	t.Run("service passes back error from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{Error: ErrFailedToUpdateInstance})
+
+		_, err := instanceService.CreateInstance(entity.Instance{})
+
+		assert.NotNil(t, err)
+	})
 }
 
 func TestService_UpdateInstance(t *testing.T) {
-	instanceService := New()
+	t.Run(
+		"service passes back instance from repository",
+		func(t *testing.T) {
+			id := uuid.New()
+			instanceService := New(repositorySpy{Instance: entity.Instance{Id: id}})
 
-	got, err := instanceService.UpdateInstance(entity.Instance{})
+			got, err := instanceService.UpdateInstance(entity.Instance{})
 
-	assert.Nil(t, err)
-	assert.Equal(t, entity.Instance{}, got)
+			assert.Nil(t, err)
+			assert.Equal(t, id, got.Id)
+		},
+	)
+
+	t.Run("service passes back error from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{Error: ErrFailedToUpdateInstance})
+
+		_, err := instanceService.UpdateInstance(entity.Instance{})
+
+		assert.NotNil(t, err)
+	})
 }
 
 func TestService_DeleteInstance(t *testing.T) {
-	instanceService := New()
-	id, _ := uuid.NewUUID()
+	t.Run("service passes back error from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{Error: ErrFailedToUpdateInstance})
 
-	err := instanceService.DeleteInstance(id)
+		err := instanceService.DeleteInstance(uuid.New())
 
-	assert.Nil(t, err)
+		assert.NotNil(t, err)
+	})
+	t.Run("service passes back nil from repository", func(t *testing.T) {
+		instanceService := New(repositorySpy{})
+
+		err := instanceService.DeleteInstance(uuid.New())
+
+		assert.Nil(t, err)
+	})
 }
