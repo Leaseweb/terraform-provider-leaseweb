d0452a6dbaa3d3527e643be5dcc3ead07542416e
Ryan Kes <ryan@ryankes.eu>
2024-07-24 17:44:04 +0200

    docs: add some documentation to the ports

--- internal/core/ports/public_cloud_repository.go
+++ internal/core/ports/public_cloud_repository.go
@@ -9,37 +9,52 @@ import (
 )
 
 type PublicCloudRepository interface {
-	GetAllInstances(ctx context.Context) (domain.Instances, *shared.RepositoryError)
+	// GetAllInstances Retrieve all instances from the public cloud api.
+	GetAllInstances(ctx context.Context) (
+		domain.Instances,
+		*shared.RepositoryError,
+	)
 
-	GetInstance(id value_object.Uuid, ctx context.Context) (*domain.Instance, *shared.RepositoryError)
+	// GetInstance Retrieve instance details from the public cloud api.
+	GetInstance(
+		id value_object.Uuid,
+		ctx context.Context,
+	) (*domain.Instance, *shared.RepositoryError)
 
+	// CreateInstance Create a new instance in the public cloud api.
 	CreateInstance(
 		instance domain.Instance,
 		ctx context.Context,
 	) (*domain.Instance, *shared.RepositoryError)
 
+	// UpdateInstance Update an instance in the public cloud api.
 	UpdateInstance(
 		instance domain.Instance,
 		ctx context.Context,
 	) (*domain.Instance, *shared.RepositoryError)
 
+	// DeleteInstance Delete an instance in the public cloud api.
 	DeleteInstance(id value_object.Uuid, ctx context.Context) *shared.RepositoryError
 
+	// GetAutoScalingGroup Get autoScalingGroup details from the public cloud api.
 	GetAutoScalingGroup(
 		id value_object.Uuid,
 		ctx context.Context,
 	) (*domain.AutoScalingGroup, *shared.RepositoryError)
 
+	// GetLoadBalancer Get load balancer details from the public cloud api.
 	GetLoadBalancer(
 		id value_object.Uuid,
 		ctx context.Context,
 	) (*domain.LoadBalancer, *shared.RepositoryError)
 
+	// GetAvailableInstanceTypesForUpdate Get all possible instances types an instance is allowed to upgrade to from the public cloud api.
 	GetAvailableInstanceTypesForUpdate(
 		id value_object.Uuid,
 		ctx context.Context,
 	) (domain.InstanceTypes, *shared.RepositoryError)
 
+	// GetRegions Get a list of all regions from the public cloud api.
 	GetRegions(
 		ctx context.Context,
 	) (domain.Regions, *shared.RepositoryError)
--- internal/core/ports/public_cloud_service.go
+++ internal/core/ports/public_cloud_service.go
@@ -9,30 +9,37 @@ import (
 )
 
 type PublicCloudService interface {
+	// GetAllInstances Get all instances.
 	GetAllInstances(ctx context.Context) (domain.Instances, *shared.ServiceError)
 
+	// GetInstance Get a single instance.
 	GetInstance(
 		id value_object.Uuid,
 		ctx context.Context,
 	) (*domain.Instance, *shared.ServiceError)
 
+	// CreateInstance Create an instance.
 	CreateInstance(
 		instance domain.Instance,
 		ctx context.Context,
 	) (*domain.Instance, *shared.ServiceError)
 
+	// UpdateInstance Update an instance.
 	UpdateInstance(
 		instance domain.Instance,
 		ctx context.Context,
 	) (*domain.Instance, *shared.ServiceError)
 
+	// DeleteInstance Delete an instance.
 	DeleteInstance(id value_object.Uuid, ctx context.Context) *shared.ServiceError
 
+	// GetAvailableInstanceTypesForUpdate Get all available instances types an instance can upgrade to.
 	GetAvailableInstanceTypesForUpdate(
 		id value_object.Uuid,
 		ctx context.Context,
 	) (domain.InstanceTypes, *shared.ServiceError)
 
+	// GetRegions Get a list of all regions.
 	GetRegions(
 		ctx context.Context,
 	) (domain.Regions, *shared.ServiceError)
