6379a6e4116a133c8e9ac78f10ffa990484bed82
Ryan Kes <ryan@ryankes.eu>
2024-10-06 16:19:08 +0200

    chore: move datasource code to data_source.go

--- internal/provider/data_sources/public_cloud/instances/data_source.go
+++ internal/provider/data_sources/public_cloud/instances/data_source.go
@@ -1,8 +1,13 @@
 package instances
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/leaseweb/terraform-provider-leaseweb/internal/provider/client"
+	"github.com/leaseweb/terraform-provider-leaseweb/internal/provider/logging"
 )
 
 var (
@@ -16,4 +21,68 @@ func NewInstancesDataSource() datasource.DataSource {
 
 type instancesDataSource struct {
 	client client.Client
+}
+
+func (d *instancesDataSource) Configure(
+	_ context.Context,
+	req datasource.ConfigureRequest,
+	resp *datasource.ConfigureResponse,
+) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	coreClient, ok := req.ProviderData.(client.Client)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf(
+				"Expected provider.Client, got: %T. Please report this issue to the provider developers.",
+				req.ProviderData,
+			),
+		)
+
+		return
+	}
+
+	d.client = coreClient
+}
+
+func (d *instancesDataSource) Metadata(
+	_ context.Context,
+	req datasource.MetadataRequest,
+	resp *datasource.MetadataResponse,
+) {
+	resp.TypeName = req.ProviderTypeName + "_public_cloud_instances"
+}
+
+func (d *instancesDataSource) Read(
+	ctx context.Context,
+	_ datasource.ReadRequest,
+	resp *datasource.ReadResponse,
+) {
+
+	tflog.Info(ctx, "Read public cloud instances")
+	instances, err := d.client.PublicCloudFacade.GetAllInstances(ctx)
+
+	if err != nil {
+		resp.Diagnostics.AddError("Unable to read instances", err.Error())
+		logging.FacadeError(
+			ctx,
+			err.ErrorResponse,
+			&resp.Diagnostics,
+			"Unable to read instances",
+			err.Error(),
+		)
+
+		return
+	}
+
+	state := instances
+
+	diags := resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
 }
