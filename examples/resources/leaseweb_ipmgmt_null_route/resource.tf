b61698729bf8c9bd50a45a851c136dee00652bdd
Ryan Kes <511318+alrayyes@users.noreply.github.com>
2025-01-14 09:52:19 +0100

    feat: add null route IP resource (#247)
    * feat: add null route ip resource
    
    ---------
    
    Co-authored-by: Majid Karimizadeh <m.karimizadeh@global.leaseweb.com>
    Co-authored-by: Imtiaz Ahmed <i.ahmed@global.leaseweb.com>

---
+++ examples/resources/leaseweb_ipmgmt_null_route/resource.tf
@@ -0,0 +1,11 @@
+# Manage null route history
+resource "leaseweb_ipmgmt_null_route" "nr" {
+  ip = "127.0.0.1"
+}
+
+# Update comment for a null route history
+resource "leaseweb_ipmgmt_null_route" "nr" {
+  ip      = "127.0.0.1"
+  comment = "this is comment"
+}
+
