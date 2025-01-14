46e8ba38e483b3a094346cd299f4090bde0d45be
Majid Karimizadeh <m.karimizadeh@global.leaseweb.com>
2025-01-14 14:33:20 +0100

    chore: update sdk for dedicated server

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -1652,123 +1652,18 @@ func TestAccDedicatedServerDataSource(t *testing.T) {
 				{
 					Config: providerConfig + `
 						data "leaseweb_dedicated_server" "test" {
-							id = "12345"
+							id = "123456"
 						}`,
 					Check: resource.ComposeAggregateTestCheckFunc(
 						resource.TestCheckResourceAttr(
 							"data.leaseweb_dedicated_server.test",
 							"asset_id",
-							"627294",
+							"1234561882",
 						),
 						resource.TestCheckResourceAttr(
 							"data.leaseweb_dedicated_server.test",
 							"contract_id",
-							"674382",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"cpu_quantity",
-							"4",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"cpu_type",
-							"Intel Xeon E3-1220",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"internal_gateway",
-							"123.123.123.126",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"internal_ip",
-							"123.123.123.123/27",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"internal_mac",
-							"AA:BB:CC:DD:EE:FF",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"is_automation_feature_available",
-							"true",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"is_ipmi_reboot_feature_available",
-							"false",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"is_power_cycle_feature_available",
-							"true",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"is_private_network_feature_available",
-							"true",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"is_remote_management_feature_available",
-							"false",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"location_rack",
-							"13",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"location_site",
-							"AMS-01",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"location_suite",
-							"A6",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"location_unit",
-							"16-17",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"public_gateway",
-							"123.123.123.126",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"public_ip",
-							"123.123.123.123/27",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"public_mac",
-							"AA:BB:CC:DD:EE:FF",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"rack_type",
-							"DEDICATED",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"ram_size",
-							"32",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"ram_unit",
-							"GB",
-						),
-						resource.TestCheckResourceAttr(
-							"data.leaseweb_dedicated_server.test",
-							"serial_number",
-							"JDK18291JK",
+							"12123412312",
 						),
 					),
 				},
@@ -2891,7 +2786,7 @@ func TestAccDedicatedServerResource(t *testing.T) {
 					ResourceName:       "leaseweb_dedicated_server.test",
 					ImportState:        true,
 					ImportStatePersist: true,
-					ImportStateId:      "12345",
+					ImportStateId:      "123456",
 				},
 				// Update and Read testing
 				{
@@ -2903,12 +2798,12 @@ func TestAccDedicatedServerResource(t *testing.T) {
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"id",
-							"12345",
+							"123456",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"reference",
-							"database.server",
+							"this is ref",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
@@ -2938,37 +2833,37 @@ func TestAccDedicatedServerResource(t *testing.T) {
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"public_ip",
-							"123.123.123.123",
+							"127.0.0.1",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"remote_management_ip",
-							"",
+							"127.0.0.1",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"internal_mac",
-							"AA:BB:CC:DD:EE:FF",
+							"macmacmac",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"location.rack",
-							"13",
+							"LK18",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"location.site",
-							"AMS-01",
+							"AMS-02",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"location.suite",
-							"A6",
+							"HALL1",
 						),
 						resource.TestCheckResourceAttr(
 							"leaseweb_dedicated_server.test",
 							"location.unit",
-							"16-17",
+							"12",
 						),
 					),
 				},
